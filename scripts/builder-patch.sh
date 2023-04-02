3b99c2141ee6e6911b8af81e57dfe90d53b23ae3
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-02 16:42:20 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -7,9 +7,9 @@ cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
 
 # Remove redundant default packages
 sed -i "/luci-app-cpufreq/d" include/target.mk
-sed -i 's/luci-app-filetransfer/luci-app-udpxy/g' include/target.mk
-sed -i 's/luci-app-turboacc/luci-app-upnp/g' include/target.mk
-sed -i 's/automount/luci-app-openclash luci-app-store/g' target/linux/x86/Makefile
+sed -i "luci-app-filetransfer/d"' include/target.mk
+sed -i "luci-app-turboacc/d" include/target.mk
+sed -i "automount/d" target/linux/x86/Makefile
 
 # Force opkg to overwrite files
 sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
@@ -19,11 +19,6 @@ sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
 # Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
-
-sed -i "s/CONFIG_PACKAGE_luci-app-filetransfer=y/# CONFIG_PACKAGE_luci-app-filetransfer is not set/" .config
-sed -i "s/CONFIG_PACKAGE_luci-app-filetransfer-zh-cn=y/# CONFIG_PACKAGE_luci-app-filetransfer-zh-cn is not set/" .config
-sed -i "s/CONFIG_PACKAGE_luci-app-turboacc=y/# CONFIG_PACKAGE_luci-app-turboacc is not set/" .config
-sed -i "s/CONFIG_PACKAGE_luci-app-turboacc-zh-cn=y/# CONFIG_PACKAGE_luci-app-turboacc-zh-cn is not set/" .config
 
 # 固件大小
 #sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=32/CONFIG_TARGET_KERNEL_PARTSIZE=80/" .config
