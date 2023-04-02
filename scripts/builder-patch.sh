3fe62a9dbd9e4956f55b98c144d171acfec5b0e3
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-02 14:39:38 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -7,6 +7,9 @@ cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
 
 # Remove redundant default packages
 sed -i "/luci-app-cpufreq/d" include/target.mk
+sed -i 's/luci-app-filetransfer/luci-app-udpxy/g' include/target.mk
+sed -i 's/luci-app-turboacc/luci-app-upnp/g' include/target.mk
+sed -i 's/automount/luci-app-openclash luci-app-store/g' target/linux/x86/Makefile
 
 # Force opkg to overwrite files
 sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
