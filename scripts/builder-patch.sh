407d17c5fb8469ae2f915291dfe5d6c83a3294e9
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-02 11:52:29 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -16,3 +16,7 @@ sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
 # Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
+
+# 固件大小
+sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=32/CONFIG_TARGET_KERNEL_PARTSIZE=80/" .config
+sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=160/CONFIG_TARGET_ROOTFS_PARTSIZE=600/" .config
