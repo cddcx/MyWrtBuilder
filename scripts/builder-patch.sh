1fb17dd8f88195ce34ce6db7f2b910f721bc09d9
cddcx <84568016+cddcx@users.noreply.github.com>
2023-04-02 12:22:19 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -17,6 +17,11 @@ sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 # Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
 
+sed -i "s/CONFIG_PACKAGE_luci-app-filetransfer=y/# CONFIG_PACKAGE_luci-app-filetransfer is not set/" .config
+sed -i "s/CONFIG_PACKAGE_luci-app-filetransfer-zh-cn=y/# CONFIG_PACKAGE_luci-app-filetransfer-zh-cn is not set/" .config
+sed -i "s/CONFIG_PACKAGE_luci-app-turboacc=y/# CONFIG_PACKAGE_luci-app-turboacc is not set/" .config
+sed -i "s/CONFIG_PACKAGE_luci-app-turboacc-zh-cn=y/# CONFIG_PACKAGE_luci-app-turboacc-zh-cn is not set/" .config
+
 # 固件大小
-sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=32/CONFIG_TARGET_KERNEL_PARTSIZE=80/" .config
-sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=160/CONFIG_TARGET_ROOTFS_PARTSIZE=600/" .config
+#sed -i "s/CONFIG_TARGET_KERNEL_PARTSIZE=32/CONFIG_TARGET_KERNEL_PARTSIZE=80/" .config
+#sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=160/CONFIG_TARGET_ROOTFS_PARTSIZE=600/" .config
